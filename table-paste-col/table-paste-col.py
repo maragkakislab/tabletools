e3e743cd93ed857df1234b5e235ea56882ec24e1
Jessica Martin <jessica.martin@nih.gov>
2021-04-16 13:07:24 -0500

    option added to place column at right end

--- table-paste-col/table-paste-col.py
+++ table-paste-col/table-paste-col.py
@@ -17,22 +17,27 @@ parser.add_argument("-t", "--table", default='-', help="input csv file; STDIN if
 parser.add_argument("-c", "--col-name", required=True, help="name for new column")
 parser.add_argument("-v", "--col-value", required=True, help="constant value for new column")
 parser.add_argument("-s", "--separator", default='\t', help="column separator character, default: <TAB>")
+parser.add_argument("--at-end", action="store_true", help="inserts new column as the right most one.")
 args = parser.parse_args()
 
 
 table = get_input_file_object(args.table)
     
-def add_col(sep, colname, colvalue):
+def add_col(sep, colname, colvalue, at_end):
     i = 0
     for line in table:
         if i==0:
             line = line.rstrip('\n')
-            print(line + sep + colname)
+            if at_end:
+                print(line + sep + colname)
+            print(colname + sep + line)
         else:
             line = line.rstrip('\n')
-            print(line + sep + colvalue)
+            if at_end:
+                print(line + sep + colvalue)
+            print(colvalue + sep + line)
         i += 1
 
     return
 
-add_col(args.separator, args.col_name, args.col_value)
+add_col(args.separator, args.col_name, args.col_value, args.at_end)
