9c1f08c02d03481f41bf039e009cc4a7a576dae6
darsa <showkat.dar@nih.gov>
2021-10-04 13:59:00 -0500

    table-join.py: merging on  multiple columns, and adding optional suffix to output columns

--- table-join/table-join.py
+++ table-join/table-join.py
@@ -15,13 +15,17 @@ parser.add_argument("-t","--table1",
                     help = "Input first table file; reads from STDIN if '-'")
 parser.add_argument("-g","--table2",
                     help = "Input second table file; reads from STDIN if '-'")
-parser.add_argument("-c","--key-table1", default= "transcript",
-                    help = "Name of column (header) first table, (default: %(default)s)")
-parser.add_argument("-d","--key-table2", default= "transcript",
-                    help = "Name of index column (header) second table, (default: %(default)s)")
+parser.add_argument("-c","--key1", nargs='+',
+                    help = "Name(s) of column (headers) first table")
+parser.add_argument("-d","--key2", nargs='+',
+                    help = "Name(s) of column (headers) second table")
+parser.add_argument("-x","--suffix1", default="x",
+                    help = "Suffix to add to overlapping column names,(default: %(default)s)")
+parser.add_argument("-y","--suffix2", default="y",
+                    help = "Suffix to add to overlapping column names,(default: %(default)s)")
 parser.add_argument("-s", "--delim", default="\t",
                     help="Delimiter to separate the columns of the output file, default : <TAB>")
-parser.add_argument("-k","--how-key", default= "inner",
+parser.add_argument("-k","--how", default= "inner",
                     help = "How to merge ‘left’, ‘right’, ‘outer’, ‘inner’, ‘cross’,(default: %(default)s)")
 args = parser.parse_args()
 
@@ -36,7 +40,7 @@ if args.table1 == "-" and  args.table2 == "-":
     print("Error: can't provide stdin twice as input", file=sys.stderr)
     sys.exit(1)
 
-#open are dataframe the two files
+#open dataframe of two tables
 ifile1 = get_filehandle(args.table1)
 df_ifile1 = pd.read_csv(ifile1, sep="\t")
 
@@ -45,7 +49,10 @@ df_ifile2 = pd.read_csv(ifile2, sep="\t")
 
 # Merge the two dataframes based on common/index column
 outfile_df = pd.merge(df_ifile1, df_ifile2,
-    left_on=args.key_table1, right_on=args.key_table2, how=args.how_key)
+             left_on=args.key1,
+             right_on=args.key2,
+             how=args.how,
+             suffixes=(args.suffix1, args.suffix2))
 
 # Output of table1 with new column(s) incld. newcolumns from table2 to terminal
 delim = args.delim
