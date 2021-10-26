8879fe6b4f1ad3ad74db64530313690bfaca0df4
Emmanouil "Manolis" Maragkakis <emmanouil.maragkakis@nih.gov>
2021-10-26 16:38:10 -0500

    table-group-summarize: support summarizing more than 1 columns

--- table-group-summarize/table-group-summarize.py
+++ table-group-summarize/table-group-summarize.py
@@ -23,29 +23,38 @@ def get_input_file_object(filename):
 
 
 parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument("-t", "--table", default='-',
+parser.add_argument("-t", "--table", required=True, default='-',
                     help="Input csv file; STDIN if - (default: %(default)s)")
-parser.add_argument("-g", "--groupby", action='append', nargs='+',
+parser.add_argument("-g", "--groupby", required=True, nargs='+',
                     help="Name of column/s to group entries by")
-parser.add_argument("-y", "--summarize",
+parser.add_argument("-y", "--summarize", required=True, nargs='+',
                     help="Name of column to summarize")
-parser.add_argument("-f", "--func", required=True, action='append', nargs='+',
+parser.add_argument("-f", "--func", required=True, nargs='+',
                     help="Pandas function/s to summarize columns e.g. mean")
 parser.add_argument("-s", "--sep", default='\t',
                     help="Column separator (default: <TAB>)")
 args = parser.parse_args()
 
 # Flatten the groupby and func options. They were lists of lists.
-groupby = args.groupby[0]
-functions = args.func[0]
+groupby = args.groupby
+functions = args.func
+summarize_cols = args.summarize
 
 # Read the data.
 table = get_input_file_object(args.table)
 df = pd.read_csv(table, sep=args.sep)
 
+summarize_dict = {}
+for col in summarize_cols:
+    summarize_dict[col] = functions
+
 # Group and summarize the data.
-grouped = df.groupby(groupby).agg({args.summarize: functions})
-grouped.columns = [args.summarize+"_"+f for f in functions]
+grouped = df.groupby(groupby).agg(summarize_dict)
+newcols = []
+for col in summarize_cols:
+    for f in functions:
+        newcols.append(col+'_'+f)
+grouped.columns = newcols
 grouped = grouped.reset_index()
 
 # Output grouped dataset.
