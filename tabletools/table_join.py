4ec81856aea394d2f267c2dcfab5da0ba0410da2
Emmanouil "Manolis" Maragkakis <emmanouil.maragkakis@nih.gov>
2021-12-02 16:29:50 -0600

    table-join: refactor and unit test

--- tabletools/table_join.py
+++ tabletools/table_join.py
@@ -1,33 +1,34 @@
-#!/usr/bin/env python
-
 """
-Join the columns of two tables based on given key columns, using
-(pandas dataframe.merge option). Assumes that the input file has a
-header line with column names.
+Join the columns of two tables based on given key columns. Assumes that the
+input files have a header with column names.
 """
 
 import argparse
 import sys
 import pandas as pd
 
-parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument("-t","--table1",
-                    help = "Input first table file; reads from STDIN if '-'")
-parser.add_argument("-g","--table2",
-                    help = "Input second table file; reads from STDIN if '-'")
-parser.add_argument("-c","--key1", nargs='+',
-                    help = "Name(s) of column (headers) first table")
-parser.add_argument("-d","--key2", nargs='+',
-                    help = "Name(s) of column (headers) second table")
-parser.add_argument("-x","--suffix1", required=False,
-                    help = "Suffix to add to overlapping column names")
-parser.add_argument("-y","--suffix2", required=False,
-                    help = "Suffix to add to overlapping column names")
-parser.add_argument("-s", "--delim", default="\t",
-                    help="Delimiter to separate the columns of the output file, default : <TAB>")
-parser.add_argument("-k","--how", default= "inner",
-                    help = "How to merge ‘left’, ‘right’, ‘outer’, ‘inner’, ‘cross’,(default: %(default)s)")
-args = parser.parse_args()
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("-t", "--table1", required=True,
+                        help="First table file; reads from STDIN if '-'")
+    parser.add_argument("-g", "--table2", required=True,
+                        help="Second table file; reads from STDIN if '-'")
+    parser.add_argument("-c", "--key1", nargs='+', required=True,
+                        help="Name of column(s) to be used as key in first table")
+    parser.add_argument("-d", "--key2", nargs='+', required=True,
+                        help="Name of column(s) to be used as key in second table")
+    parser.add_argument("-x", "--suffix1", default="_x",
+                        help="Suffix for overlapping column names (default: %(default)s)")
+    parser.add_argument("-y", "--suffix2", default="_y",
+                        help="Suffix for overlapping column names (default: %(default)s)")
+    parser.add_argument("-s", "--delim", default="\t",
+                        help="Delimiter to separate output columns, default : <TAB>")
+    parser.add_argument("-k", "--how", default="inner",
+                        choices=['left', 'right', 'outer', 'inner', 'cross'],
+                        help="Type of join, (default: %(default)s)")
+    return parser.parse_args(args)
+
 
 def get_filehandle(filename):
     """ Returns a filehandle """
@@ -35,30 +36,31 @@ def get_filehandle(filename):
         return sys.stdin
     return open(filename, "r")
 
-#Condition for NOT taking both input files (file1 & file2) via stdin
-if args.table1 == "-" and  args.table2 == "-":
-    print("Error: can't provide stdin twice as input", file=sys.stderr)
-    sys.exit(1)
 
-#open dataframe of two tables
-ifile1 = get_filehandle(args.table1)
-df_ifile1 = pd.read_csv(ifile1, sep="\t")
+def join(ifile1, ifile2, key1, key2, suffixes=('_x', '_y'), how='inner'):
 
-ifile2 = get_filehandle(args.table2)
-df_ifile2 = pd.read_csv(ifile2, sep="\t")
+    df1 = pd.read_csv(ifile1, sep="\t")
+    df2 = pd.read_csv(ifile2, sep="\t")
 
-# Merge the two dataframes based on common/index column
-if args.suffix1 and args.suffix2:
-    outfile_df = pd.merge(df_ifile1, df_ifile2,
-                 left_on=args.key1,
-                 right_on=args.key2,
-                 how=args.how,
-                 suffixes=(args.suffix1, args.suffix2))
-else:
-    outfile_df = pd.merge(df_ifile1, df_ifile2,
-                 left_on=args.key1,
-                 right_on=args.key2,
-                 how=args.how)
-# Output of table1 with new column(s) incld. newcolumns from table2 to terminal
-delim = args.delim
-outfile_df.to_csv(sys.stdout, index=False, sep=delim)
+    return pd.merge(df1, df2, left_on=key1, right_on=key2, how=how,
+                    suffixes=suffixes)
+
+
+def main():
+    args = parse_args(sys.argv[1:])
+
+    # Make sure that at least one file does not read from stdin.
+    if args.table1 == "-" and args.table2 == "-":
+        print("Error: '-' provided as input for both tables", file=sys.stderr)
+        sys.exit(1)
+
+    ifile1 = get_filehandle(args.table1)
+    ifile2 = get_filehandle(args.table2)
+
+    df = join(ifile1, ifile2, args.key1, args.key2,
+              [args.suffix1, args.suffix2], args.how)
+
+    ifile1.close()
+    ifile2.close()
+
+    df.to_csv(sys.stdout, index=False, sep=args.delim)
